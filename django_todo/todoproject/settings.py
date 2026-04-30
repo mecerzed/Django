9bab5f5b34620b872a15b77b7fab537737e6d2bb
mecerzed <mecertinashe@gmail.com>
2026-04-30 19:44:28 +0200

    Correct ALLOWED_HOSTS for Render

--- django_todo/todoproject/settings.py
+++ django_todo/todoproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-^-zp=-=++(-1jq%42pbp4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='.render.com,localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='.onrender.com,localhost,127.0.0.1').split(',')
 
 
 # Application definition
